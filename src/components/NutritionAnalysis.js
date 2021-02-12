f2defb567cb808cf4f5f252412742a02ae41e05d
tejaswini22199 <tejaswini22199@gmail.com>
2021-02-12 11:54:35 +0530

    m

--- src/components/NutritionAnalysis.js
+++ src/components/NutritionAnalysis.js
@@ -1,17 +1,23 @@
 import React from 'react'
-import {useEffect} from 'react'
+import {useEffect,useState} from 'react'
+import axios from 'axios'
 const NutritionAnalysis = () => {
-    const [nutriget,setNutriget]=useState('');
-    useEffect(()=>
-    {
-        const fetchItems=async()=>
-        {
-            const response=await axios(`https://api.edamam.com/api/nutrition-data?app_id=${process.env.app_id}&app_key=${process.env.app_key}&ingr=one%20large%20apple`);
-            console.log(response.data);
-           
-        }
-        fetchItems();
-    },[]);
+//     const [nutriget,setNutriget]=useState('');
+//     useEffect(()=>
+//     {
+//         const response = 
+//         await fetch(`https://api.edamam.com/api/nutrition-data?app_id=${process.env.app_id}&app_key=${process.env.app_key}&ingr=1%20large%20apple`, {
+//         method: "GET",
+//         mode: "cors",
+//         headers: {
+//             // "Authorization": `Bearer: ${api_key}`,
+//             "Content-Type": "application/json"
+//         },
+//     body: JSON.stringify(data)
+// });
+// console.log(response.json())
+//         // fetchItems();
+//     },[]);
     return (
         <div>
             <h1>GG</h1>
